fd70efeecf957909ec9574fceec31e6affdbfda5
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2022-09-06 13:00:01 -0400

    update

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 
-
-for (let i = 0; i <7; i++){
+for (let i = 0; i < 7; i++){
     for (let j = 0; j < 3; j++){
-        if( j==0 || j==i + 1 || i + j==4){
+        if (j==0 || j == i + 1 || i + j ==4){
             document.write(" X")
         } else {
             document.write(" &nbsp ")
